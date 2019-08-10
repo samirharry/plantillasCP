fe1e8e45feac4c174f6882b82cce54d3781f5605
Samir Harry Huaman Patricio <shuamanp@uni.pe>
2019-08-10 18:56:53 -0500

    a

--- Algebra/Fundamentals/Extended Eculidean Algorithm/extendedEuclideanAlgorithm.cpp
+++ Algebra/Fundamentals/Extended Eculidean Algorithm/extendedEuclideanAlgorithm.cpp
@@ -7,23 +7,20 @@ typedef long long ll;
 typedef long double ld;
 using namespace std;
 
-
 /*
-	Teniendo dos numeros a y b;
-	sea d el minimo comun multplo de a y b entonces es posible encontrar dos numeros enteros tal
-	que a*x+b*y = d
-
+	d es el mcd de a y b
+	entonces a*x+b*y = d;
 */
 
-ll gcdEuclidesExtended(ll a, ll b, ll &x, ll &y){
-	if(a ==0){
+ll gcdEuclideanExtended(ll a, ll b, ll &x, ll &y){
+	if(a == 0){
 		x = 0;
-		y = 1;
+		y=1;
 		return b;
 	}
 	ll x1,y1;
-	ll d = gcdEuclidesExtended(b%a,a,x1,y1);
-	x = y1 -(b/a)*x1;
+	ll d = gcdEuclideanExtended(b%a,a,x1,y1);
+	x = y1-(b/a)*x1;
 	y = x1;
 	return d;
 }
