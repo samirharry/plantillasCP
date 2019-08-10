8ca0fa704a7d8f715988c3283becd1a57234f7c0
Samir Harry Huaman Patricio <shuamanp@uni.pe>
2019-08-10 14:44:19 -0500

    GCD Euclides Agregado

--- Algebra/Fundamentals/EuclideanAlgorithmGCD/euclideanGCD.cpp
+++ Algebra/Fundamentals/EuclideanAlgorithmGCD/euclideanGCD.cpp
@@ -7,8 +7,28 @@ typedef long long ll;
 typedef long double ld;
 using namespace std;
 
-int main(){
-	
-	
-	return 0;
+
+/*
+	a>b;
+	complexity O(log(min(a,b)))
+*/
+
+ll gcdRecursive(ll a, ll b);
+ll gcdIterative(ll a, ll b);
+ll lcm(ll a, ll b);
+
+ll gcdRecursive(ll a, ll b){
+	return b?gcdRecursive(b,a%b):a;
+}
+
+ll gcdIterative(ll a, ll b){
+	while(b){
+		a%=b;
+		swap(a,b);
+	}
+	return a;
+}
+
+ll lcm(ll a, ll b){
+	return a/(gcdIterative(a,b))*b;
 }
