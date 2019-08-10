0bbc9f684fde839cd7f93de9a9fd74d094bbe28b
Samir Harry Huaman Patricio <shuamanp@uni.pe>
2019-08-10 14:20:29 -0500

    a

---
+++ Algebra/Fundamentals/EuclideanAlgorithmGCD/euclideanGCD.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define debug(x) cout<<#x<<" => "<<x<<endl;
+#define debugArray(x,n) for(ll i=0;i<n;i++) cout<<#x<<"["<<i<<"] => "<<x[i]<<endl;
+
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+
+int main(){
+	
+	
+	return 0;
+}
